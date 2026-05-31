8e1bd8aeeeaa25cda89f6ada15b38723654174dd
ArticlesJoey <joey@articles.media>
2026-05-31 09:15:23 -0400

    Package updates and minimal dev-box games list

---
+++ app/api/games/articles-media/dev-box/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+import games from '@/components/constants/games';
+
+// Used for articles-dev-box GamesDropdown
+
+export async function GET(req) {
+
+    const response = NextResponse.json(
+        games
+        .filter(game => game.public !== false)
+        .filter(game => game.preview !== true)
+        .map(game => ({
+            link: game.link,
+            name: game.name,
+        }))
+    );
+    const allowedOrigins = [
+        'http://localhost:3048',
+        'https://games.articles.media',
+        'https://articles.media'
+    ];
+    const origin = req.headers.get('origin');
+    if (allowedOrigins.includes(origin)) {
+        response.headers.set('Access-Control-Allow-Origin', origin);
+    }
+    response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return response;
+
+}

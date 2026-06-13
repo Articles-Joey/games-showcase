ddd7a33b1a8ea51a9485fc9f88706c72915069b7
ArticlesJoey <joey@articles.media>
2026-06-13 02:53:48 -0400

    GA added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import theme from '@/components/theme';
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -32,10 +35,11 @@ export default function RootLayout({ children }) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Road+Rage&display=swap" rel="stylesheet" /> */}
         <link href="https://fonts.googleapis.com/css2?family=Tiny5&display=swap" rel="stylesheet"></link>
 
       </head>
+
+      <GoogleAnalytics gaId="G-BSWV4HR4VG" />
 
       <body
         id="carousel-game-page"
